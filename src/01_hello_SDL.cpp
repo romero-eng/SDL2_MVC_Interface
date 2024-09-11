751304ccf129e7c03a21dc0d2defed7ad2e4f4c0
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-09-11 17:37:49 -0400

    Update 01_hello_SDL.cpp

--- src/01_hello_SDL.cpp
+++ src/01_hello_SDL.cpp
@@ -48,7 +48,7 @@ int main( int argc, char* args[] )
 		bool quit = false;
 		while(quit == false)
 		{
-			while(SDL::PollEvent(current_event))
+			if(SDL::PollEvent(current_event))
 			{
 				quit |= current_event.type == SDL::EventTypes::QUIT;
 			}
