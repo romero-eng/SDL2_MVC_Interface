4bd86711106357aadaee3e64b538f0c1eab081ba
root <root@DESKTOP-G3SKC0I.>
2024-09-26 11:53:29 -0400

    Continuation of last commit

---
+++ src/SDML/Window.cpp
@@ -0,0 +1,125 @@
+#if defined(__linux__) || defined(_WIN32) || defined(__APPLE__)
+#include "Window.hpp"
+
+SDML::Video::Window::Window(const char *title,
+				   			int width,
+                   			int height): Window(title,
+				   					   			SDL_WINDOWPOS_UNDEFINED,
+									   			SDL_WINDOWPOS_UNDEFINED,
+									   			width,
+									   			height,
+									  	 		0) {}
+
+
+SDML::Video::Window::Window(const char *title,
+				   			int x, int y,
+				   			int width,
+                   			int height): Window(title,
+				   					  			x, y,
+									   			width,
+									   			height,
+									   			0) {}
+
+
+SDML::Video::Window::Window(const char *title,
+				   			int width,
+                   			int height,
+				   			WindowFlag flag): Window(title,
+				   									 SDL_WINDOWPOS_UNDEFINED,
+													 SDL_WINDOWPOS_UNDEFINED,
+										 			 width,
+										 			 height,
+										 			 std::to_underlying(flag)) {}
+
+
+SDML::Video::Window::Window(const char *title,
+		                   	int x, int y,
+						   	int width,
+                		   	int height,
+				   			WindowFlag flag): Window(title,
+				   									 x, y,
+										 			 width,
+										 			 height,
+										 			 std::to_underlying(flag)) {}
+
+
+SDML::Video::Window::Window(const char *title,
+						   	int width,
+        		           	int height,
+						   	Uint32 flags): Window(title,
+				   						 	 	  SDL_WINDOWPOS_UNDEFINED,
+										 		  SDL_WINDOWPOS_UNDEFINED,
+										 		  width,
+										 		  height,
+										 		  flags) {}
+
+
+SDML::Video::Window::Window(const char *title,
+                   			int x, int y,
+						    int width,
+                   			int height,
+				   			Uint32 flags): internal_SDL_window{SDL_CreateWindow(title,
+				   															    x, y,
+																	   			width,
+																	   			height,
+																	   			flags)} {}
+
+
+void SDML::Video::Window::Flash(FlashOperation operation)
+{
+	SDL_FlashOperation internal_SDL_operation;
+	switch(operation)
+	{
+		case FlashOperation::CANCEL:
+			internal_SDL_operation = SDL_FLASH_CANCEL;
+            break;
+		case FlashOperation::BRIEFLY:
+			internal_SDL_operation = SDL_FLASH_BRIEFLY;
+            break;
+		case FlashOperation::UNTIL_FOCUSED:
+			internal_SDL_operation = SDL_FLASH_UNTIL_FOCUSED;
+            break;
+	}
+
+	if(SDL_FlashWindow(this->internal_SDL_window, internal_SDL_operation) < 0)
+	{
+		throw fmt::format("Could not Flash the '{:s}' Window: {:s}",
+						  SDL_GetWindowTitle(this->internal_SDL_window),
+						  SDL_GetError());
+	}
+}
+
+
+SDML::Video::Window::~Window()
+{
+	if(SDL_HasWindowSurface(this->internal_SDL_window))
+	{
+		if(SDL_DestroyWindowSurface(this->internal_SDL_window) < 0)
+		{
+			std::cerr << fmt::format("Could not Destroy Surface for '{:s}' Window: {:s}",
+									 SDL_GetWindowTitle(this->internal_SDL_window),
+									 SDL_GetError());
+		}
+	}
+	SDL_DestroyWindow(this->internal_SDL_window);
+}
+
+
+Uint32 operator|(const SDML::Video::WindowFlag& first_flag,
+				 const SDML::Video::WindowFlag& second_flag)
+{ return std::to_underlying(first_flag) | std::to_underlying(second_flag) ; }
+
+
+Uint32 operator|(const SDML::Video::WindowFlag& first_flag,
+				 Uint32 second_flag)
+{ return std::to_underlying(first_flag) | second_flag ; }
+
+
+Uint32 operator|(Uint32 first_flag,
+				 const SDML::Video::WindowFlag& second_flag)
+{ return first_flag | std::to_underlying(second_flag) ; }
+
+
+#else
+#error "This file is only meant to be compiled on a Windows, Macintosh, or Linux OS"
+#endif
---
+++ src/SDML/Window.hpp
@@ -0,0 +1,115 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+// Third-party Libraries
+#include <SDL2/SDL.h>
+#include <fmt/format.h>
+
+// C++ Standard Libraries
+#include <iostream>
+#include <utility>
+
+namespace SDML
+{
+	namespace Video
+	{
+		enum class WindowFlag: uint32_t
+		{
+			FULLSCREEN    	   = SDL_WINDOW_FULLSCREEN,
+			OPENGL 	      	   = SDL_WINDOW_OPENGL,
+			SHOWN 	      	   = SDL_WINDOW_SHOWN,
+			HIDDEN 	      	   = SDL_WINDOW_HIDDEN,
+			BORDERLESS    	   = SDL_WINDOW_BORDERLESS,
+			RESIZABLE     	   = SDL_WINDOW_RESIZABLE,
+			MINIMIZED     	   = SDL_WINDOW_MINIMIZED,
+			MAXIMIZED  	  	   = SDL_WINDOW_MAXIMIZED,
+			MOUSE_GRABBED 	   = SDL_WINDOW_MOUSE_GRABBED,
+			INPUT_FOCUS   	   = SDL_WINDOW_INPUT_FOCUS,
+			MOUSE_FOCUS   	   = SDL_WINDOW_MOUSE_FOCUS,
+			FULLSCREEN_DESKTOP = SDL_WINDOW_FULLSCREEN_DESKTOP,
+			FOREIGN 		   = SDL_WINDOW_FOREIGN,
+			ALLOW_HIGHDPI 	   = SDL_WINDOW_ALLOW_HIGHDPI,
+			MOUSE_CAPTURE	   = SDL_WINDOW_MOUSE_CAPTURE,
+			ALWAYS_ON_TOP 	   = SDL_WINDOW_ALWAYS_ON_TOP,
+			SKIP_TASKBAR 	   = SDL_WINDOW_SKIP_TASKBAR,
+			UTILITY 		   = SDL_WINDOW_UTILITY,
+			TOOLTIP 		   = SDL_WINDOW_TOOLTIP,
+			POPUP_MENU 		   = SDL_WINDOW_POPUP_MENU,
+			KEYBOARD_GRABBED   = SDL_WINDOW_KEYBOARD_GRABBED,
+			VULKAN 			   = SDL_WINDOW_VULKAN,
+			METAL 			   = SDL_WINDOW_METAL,
+			INPUT_GRABBED 	   = SDL_WINDOW_INPUT_GRABBED
+		};
+
+		enum class FlashOperation
+		{
+    		CANCEL,
+    		BRIEFLY,
+    		UNTIL_FOCUSED
+		};
+
+		class Window
+		{
+		private:
+
+			SDL_Window* internal_SDL_window;
+
+		public:
+
+			Window(const char *title,
+				   int width,
+                   int height);
+
+			Window(const char *title,
+				   int x, int y,
+				   int width,
+                   int height);
+
+			Window(const char *title,
+				   int width,
+                   int height,
+				   WindowFlag flag);
+
+			Window(const char *title,
+                   int x, int y,
+				   int width,
+                   int height,
+				   WindowFlag flag);
+
+			Window(const char *title,
+				   int width,
+                   int height,
+				   Uint32 flags);
+
+			Window(const char *title,
+                   int x, int y,
+				   int width,
+                   int height,
+				   Uint32 flags);
+
+			Window(Window&) = delete;
+			
+			Window& operator=(Window&) = delete;
+
+			/*Not sure this function actually does anything, but I'll
+			leave it for the sake of completeness.*/
+			void Flash(FlashOperation operation);
+
+			~Window();
+		};
+	}
+}
+
+
+Uint32 operator|(const SDML::Video::WindowFlag& first_flag,
+				 const SDML::Video::WindowFlag& second_flag);
+
+
+Uint32 operator|(const SDML::Video::WindowFlag& first_flag,
+				 Uint32 second_flag);
+
+
+Uint32 operator|(Uint32 first_flag,
+				 const SDML::Video::WindowFlag& second_flag);
+
+#endif
