eafe3697731b2ba8fa78ccdae0330e8218f8fe70
root <root@DESKTOP-G3SKC0I.>
2024-10-04 12:56:16 -0400

    Now printing out Render Driver details upon initialization of Video Subsystem

--- src/SDML/Subsystem.cpp
+++ src/SDML/Subsystem.cpp
@@ -43,12 +43,32 @@ void SDML::Subsystem::Initialize(uint32_t subsystems)
 		video_init_msgs.add_printable("Available Video Drivers", video_drivers);
 		video_init_msgs.add_printable(   "Current Video Driver", SDL_GetCurrentVideoDriver());
 
-		SDL_RendererInfo current_driver_info {};
+		SDL_RendererInfo current_render_driver_info {};
+		Misc::Printables printable_render_driver_info;
+
 		for(std::size_t render_driver_index = 0; render_driver_index < num_render_drivers; render_driver_index++) {
-			if(SDL_GetRenderDriverInfo(static_cast<int>(render_driver_index), &current_driver_info) < 0) {
+			if(SDL_GetRenderDriverInfo(static_cast<int>(render_driver_index), &current_render_driver_info) < 0) {
 				throw std::runtime_error(fmt::format("Could not retrieve information for Render Driver #{:d}: {:s}",
 													 render_driver_index,
 													 SDL_GetError()));
+			} else {
+
+				std::size_t num_texture_formats {static_cast<std::size_t>(current_render_driver_info.num_texture_formats)};
+
+				printable_render_driver_info.clear();
+				printable_render_driver_info.set_title(fmt::format("'{:s}' Render Driver", current_render_driver_info.name));
+				for(std::size_t render_format_index = 0; render_format_index < num_texture_formats; render_format_index++){
+					printable_render_driver_info.add_printable(fmt::format("Texture Format #{:d}",
+																		   render_format_index + 1),
+															   SDL_GetPixelFormatName(current_render_driver_info.texture_formats[0]));
+				}
+				printable_render_driver_info.add_printable( 	   "Maximum Texture Width", current_render_driver_info.max_texture_width);
+				printable_render_driver_info.add_printable(		  "Maximum Texture Height", current_render_driver_info.max_texture_height);
+				printable_render_driver_info.add_printable(  		"Is Software Fallback", SDL_RENDERER_SOFTWARE == (SDL_RENDERER_SOFTWARE & current_render_driver_info.flags));
+				printable_render_driver_info.add_printable(  	   		  "Is Accelerated", SDL_RENDERER_ACCELERATED == (SDL_RENDERER_ACCELERATED & current_render_driver_info.flags));
+				printable_render_driver_info.add_printable(  		  		 "VSync is On", SDL_RENDERER_PRESENTVSYNC == (SDL_RENDERER_PRESENTVSYNC & current_render_driver_info.flags));
+				printable_render_driver_info.add_printable("Supports rendering to texture", SDL_RENDERER_TARGETTEXTURE == (SDL_RENDERER_TARGETTEXTURE & current_render_driver_info.flags));
+				video_init_msgs.copy_into_printables(printable_render_driver_info);
 			}
 		}
 
