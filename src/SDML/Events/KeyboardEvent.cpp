9f7d436a87294428aeb1c7b722216f61de50e251
root <root@DESKTOP-G3SKC0I.>
2024-10-25 11:22:16 -0400

    Finished up the Keyboard Event class

--- src/SDML/Events/KeyboardEvent.cpp
+++ src/SDML/Events/KeyboardEvent.cpp
@@ -2,27 +2,119 @@
 #include "KeyboardEvent.hpp"
 
 
+SDML::Events::KeyboardEvent::Modifier SDML::Events::KeyboardEvent::SDL_to_SDML(uint16_t sdl_mod)
+{
+    Modifier mod;
+
+    switch(sdl_mod) {
+        case 0x0000:
+            mod = Modifier::NONE;
+            break;
+        case 0x0001:
+            mod = Modifier::LSHIFT;
+            break;
+        case 0x0002:
+            mod = Modifier::RSHIFT;
+            break;
+        case 0x0040:
+            mod = Modifier::LCTRL;
+            break;
+        case 0x0080:
+            mod = Modifier::RCTRL;
+            break;
+        case 0x0100:
+            mod = Modifier::LALT;
+            break;
+        case 0x0200:
+            mod = Modifier::RALT;
+            break;
+        case 0x0400:
+            mod = Modifier::LGUI;
+            break;
+        case 0x0800:
+            mod = Modifier::RGUI;
+            break;
+        case 0x1000:
+            mod = Modifier::NUM;
+            break;
+        case 0x2000:
+            mod = Modifier::CAPS;
+            break;
+        case 0x4000:
+            mod = Modifier::MODE;
+            break;
+        case 0x8000:
+            mod = Modifier::SCROLL;
+            break;
+    }
+
+    return mod;
+}
+
+
 SDML::Events::KeyboardEvent::KeyboardEvent(const SDL_Event& event,
                                            const std::chrono::time_point<std::chrono::system_clock> init_time_point): Event(event, init_time_point, false),
                                                                                                                       window{Video::FindWindow(event.key.windowID)},
                                                                                                                       pressed{event.key.state == SDL_PRESSED},
-                                                                                                                      repeat{event.key.repeat}
-{
-
-    std::array<std::string, 2> codes {fmt::format("Scancode: {:#x}", event.key.keysym.scancode),
-                                      fmt::format(" Unicode: {:#x}", event.key.keysym.sym)};
-
-    MainLogFile.Write(this->to_string());
-    std::cout << fmt::format("\n{:s}\n", fmt::join(codes, "\n")) << std::endl;
-}
+                                                                                                                      scancode{SDL_GetScancodeName(event.key.keysym.scancode)},
+                                                                                                                      unicode{SDL_GetKeyName(event.key.keysym.sym)},
+                                                                                                                      repeat{event.key.repeat},
+                                                                                                                      mod{this->SDL_to_SDML(event.key.keysym.mod)}
+{ MainLogFile.Write(this->to_string()); }
 
 
 std::string SDML::Events::KeyboardEvent::to_string() const
 {
-    Logging::Printables event_description {fmt::format("Unfinished Key {:s} Event in '{:s}' Window",
+    std::string mod_string;
+    switch (this->mod) {
+        case Modifier::NONE:
+            mod_string = "None";
+            break;
+        case Modifier::LSHIFT:
+            mod_string = "Left Shift";
+            break;
+        case Modifier::RSHIFT:
+            mod_string = "Right Shift";
+            break;
+        case Modifier::LCTRL:
+            mod_string = "Left Control";
+            break;
+        case Modifier::RCTRL:
+            mod_string = "Right Control";
+            break;
+        case Modifier::LALT:
+            mod_string = "Left Alternate";
+            break;
+        case Modifier::RALT:
+            mod_string = "Right Alternate";
+            break;
+        case Modifier::LGUI:
+            mod_string = "Left GUI";
+            break;
+        case Modifier::RGUI:
+            mod_string = "Right GUI";
+            break;
+        case Modifier::NUM:
+            mod_string = "Number";
+            break;
+        case Modifier::CAPS:
+            mod_string = "CAPS";
+            break;
+        case Modifier::MODE:
+            mod_string = "Mode";
+            break;
+        case Modifier::SCROLL:
+            mod_string = "Scroll";
+            break;
+    }
+
+    Logging::Printables event_description {fmt::format("'{:s}' Key {:s} in '{:s}' Window",
+                                                       this->unicode,
                                                        this->pressed ? "Pressed" : "Released",
                                                        this->window.GetTitle())};
+    event_description.add_printable("Scancode", this->scancode);
     event_description.add_printable("Repeats", this->repeat);
+    event_description.add_printable("Modifier", mod_string);
 
     return fmt::format("{:s}\n", event_description.print());
 }
--- src/SDML/Events/KeyboardEvent.hpp
+++ src/SDML/Events/KeyboardEvent.hpp
@@ -12,6 +12,7 @@
 
 // C++ Standard Libraries
 #include <chrono>
+#include <string>
 
 namespace SDML
 {
@@ -19,11 +20,37 @@ namespace SDML
     {
         class KeyboardEvent: public Event
         {
+        public:
+
+            enum class Modifier
+            {
+                NONE,
+                LSHIFT,
+                RSHIFT,
+                LCTRL,
+                RCTRL,
+                LALT,
+                RALT,
+                LGUI,
+                RGUI,
+                NUM,
+                CAPS,
+                MODE,
+                SCROLL
+            };
+
         private:
+
+            std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter {};
 
             Video::Window window;
             bool pressed;
+            std::string scancode;
+            std::string unicode;
             uint8_t repeat;
+            Modifier mod;
+
+            Modifier SDL_to_SDML(uint16_t sdl_mod);
 
         public:
 
