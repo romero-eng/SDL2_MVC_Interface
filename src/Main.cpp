4cf04648cbbc435666acf8a9a6b8503fd1eeaab6
root <root@DESKTOP-G3SKC0I.>
2024-10-30 22:45:45 -0400

    Implemented Custom Line Drawing Algorithm

--- src/Main.cpp
+++ src/Main.cpp
@@ -19,8 +19,94 @@
 
 constexpr std::string LOGFILE_NAME {"Test"};
 constexpr std::string WINDOW_TITLE {"Test"};
-constexpr std::string WINDOW_TITLE_2 {"Test 2"};
 constexpr std::array<int, 2> WINDOW_AREA {640, 480};
+
+constexpr int difference {5};
+constexpr std::array<std::array<int, 2>, 2> line {{{4, 200}, {300, 6}}};
+constexpr std::array<std::array<int, 2>, 2> modified_line {{{line[0][0], line[0][1] + difference},
+															{line[1][0], line[1][1] + difference}}};
+constexpr std::array<uint8_t, 4> WHITE {0xFF, 0xFF, 0xFF, 0xFF};
+
+
+std::vector<std::array<int, 2>> custom_line_drawing(const std::array<std::array<int, 2>, 2> line)
+{
+	/*
+	Problem Definition:
+	-------------------
+
+		Draw a discretized version of the line defined by the points {(x1, y1), (x1, y1)}
+		Assume a non-steep slope (slope < 1)
+
+	Algorithm:
+	----------
+
+		Δx = x2 - x1
+
+		Δy = y2 - y1
+
+		for n=1 to n=N:
+		---------------
+
+			x[n + 1] = x[n] + sgn(Δx), where x[0] = x_1 and x[N + 1] = x_2
+
+						__
+					   |
+					   |  y[n] + sgn(Δy) ,  sgn(Δy)[y(x[n + 1]) - y[n]] >= 0.5
+			y[n + 1] = |														, where y[0] = y1 and y[N + 1] = y2
+					   |	  y[n]		 , 	sgn(Δy)[y(x[n + 1]) - y[n]] < 0.5
+					   |__
+
+	Computational Optimization:
+	---------------------------
+
+		sgn(Δy)[y(x[n + 1]) - y[n]] >= 0.5  --------->  2[(n + 1)Δy - (N + 1)*Δy[n]] >= sgn(Δy)*(N + 1), where Δy[n] = y[n] - y[0]
+
+	For non-steep slopes:
+	---------------------
+
+		Switch the x and y-axes within the algorithm
+
+	*/
+
+	const auto& [point_1, point_2] = line;
+    const auto& [x1, y1] = point_1;
+    const auto& [x2, y2] = point_2;
+
+    int delta_y {y2 - y1};
+    int delta_x {x2 - x1};
+	int sgn_delta_y {y2 >= y1 ? 1 : -1};
+	int sgn_delta_x {x2 >= x1 ? 1 : -1};
+    bool steep_slope {std::abs(delta_y) > std::abs(delta_x)};
+    std::size_t N {static_cast<std::size_t>(steep_slope ? std::abs(delta_y) - 1 : std::abs(delta_x) - 1)};
+
+    std::vector<std::array<int, 2>> points(N + 2);
+    points[0] = point_1;
+    points[N + 1] = point_2;
+
+    int tmp_n_plus_one;
+    int tmp_N_plus_one {static_cast<int>(N) + 1};
+ 
+    for(std::size_t n = 0; n < N; n++) {
+
+        points[n + 1] = points[n];
+        tmp_n_plus_one = static_cast<int>(n) + 1;
+
+        if(steep_slope) {
+            if ((std::abs(tmp_n_plus_one*delta_x - tmp_N_plus_one*(points[n][0] - points[0][0])) << 1) >= tmp_N_plus_one) {
+                points[n + 1][0] += sgn_delta_x;
+            }
+            points[n + 1][1] += sgn_delta_y;
+        } else {
+            if ((std::abs(tmp_n_plus_one*delta_y - tmp_N_plus_one*(points[n][1] - points[0][1])) << 1) >= tmp_N_plus_one) {
+                points[n + 1][1] += sgn_delta_y;
+            }
+            points[n + 1][0] += sgn_delta_x;
+        }
+    }
+
+    return points;
+}
+
 
 int main( int argc, char* args[] )
 {
@@ -29,8 +115,13 @@ int main( int argc, char* args[] )
 
 	try {
 
-		SDML::Video::Window test_window {WINDOW_TITLE, WINDOW_AREA};
-		SDML::Video::Window test_window_2 {WINDOW_TITLE_2, WINDOW_AREA};
+		SDML::Video::Window canvas {WINDOW_TITLE, WINDOW_AREA};
+		SDML::Video::Renderer paintbrush {canvas};
+		paintbrush.SetBlendMode(SDML::Video::Blending::Mode::REPLACE);
+		paintbrush.SetDrawingColor(WHITE);
+		paintbrush.DrawPoints(custom_line_drawing(line));
+		paintbrush.DrawLine(modified_line);
+		paintbrush.Update();
 		
 		std::optional<std::unique_ptr<SDML::Events::Event>> current_event;
 		bool quit = false;
