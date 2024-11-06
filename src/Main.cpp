ecb13d1336fc46bce7c86bdf6a2c8c4c54d1f9f9
root <root@DESKTOP-G3SKC0I.>
2024-11-05 22:46:37 -0500

    Unfinished implementation of even-odd algorithm

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,6 +15,7 @@
 #include <filesystem>
 #include <iostream>
 #include <memory>
+#include <deque>
 
 
 constexpr std::string LOGFILE_NAME {"Test"};
@@ -22,6 +23,7 @@ constexpr std::string WINDOW_TITLE {"Test"};
 constexpr std::array<int, 2> WINDOW_AREA {640, 480};
 constexpr std::array<uint8_t, 3> WHITE {0xFF, 0xFF, 0xFF};
 constexpr std::array<uint8_t, 3> BLACK {0x00, 0x00, 0x00};
+constexpr std::array<uint8_t, 3> RED {0xFF, 0x00, 0x00};
 
 
 std::array<double, 2> operator+(const std::array<double, 2>& first_vector,
@@ -193,6 +195,154 @@ std::vector<std::array<int, 2>> custom_calculate_polygon_boundary_points(const s
 }
 
 
+std::vector<std::array<int, 2>> custom_scan_lines_for_within_shape_points(bool orthogonal_axis_first,
+                                                                          const std::vector<std::array<int, 2>>& boundary_points,
+																		  const std::optional<std::vector<std::array<int, 2>>>& orthogonal_axis_points = std::nullopt)
+{
+
+	// Initialization of numerous variables needed for up-coming for loops
+
+    bool not_found;
+    bool boundary_crossed_from_back;
+    bool boundary_crossed_from_front;
+
+    std::vector<std::array<int, 2>> within_shape_points;
+    int num_boundary_crossings;
+    int num_boundary_crossings_reversed;
+
+    std::size_t orthogonal_axis {static_cast<std::size_t>(orthogonal_axis_first ?  0 : 1)};
+    std::size_t scanning_axis {static_cast<std::size_t>(orthogonal_axis_first ? 1 : 0)};
+
+	std::size_t min_boundary_point;
+	std::size_t max_boundary_point;
+	bool keep_going;
+
+	// Reorganize the boundary points from coordinates into scan line boundary points
+	// mapped to corresponding orthogonal axis points
+    std::map<int, std::vector<int>> reorganized_boundary_points;
+    for(std::array<int, 2> point : boundary_points) {
+        if(reorganized_boundary_points.contains(point[orthogonal_axis])) {
+            reorganized_boundary_points[point[orthogonal_axis]].push_back(point[scanning_axis]);
+        } else {
+            reorganized_boundary_points.emplace(point[orthogonal_axis], std::vector<int> {point[scanning_axis]});
+        }
+    }
+
+	// If currently existing filled-in shape points were given, reorganize them from
+	// points from coordinates into scan line boundary points mapped to corresponding
+	// orthogonal axis points
+	std::map<int, std::vector<int>> reorganized_orthogonal_axis_points;
+	if(orthogonal_axis_points.has_value()) {
+		for(std::array<int, 2> point : orthogonal_axis_points.value()){
+			if(reorganized_orthogonal_axis_points.contains(point[orthogonal_axis])) {
+				reorganized_orthogonal_axis_points[point[orthogonal_axis]].push_back(point[scanning_axis]);
+			} else {
+				reorganized_orthogonal_axis_points.emplace(point[orthogonal_axis], std::vector<int> {point[scanning_axis]});
+			}
+		}
+		within_shape_points = orthogonal_axis_points.value();
+	}
+
+	// For each orthogonal axis point and it's corresponding scan line boundary points,...
+    for(auto& [orthogonal_axis_constant, scanning_axis_boundary_points] : reorganized_boundary_points) {
+
+        std::vector<std::deque<int>> boundaries;
+
+		// Make sure the scan line boundary points are not repeated/duplicated
+
+        std::sort(scanning_axis_boundary_points.begin(),
+                  scanning_axis_boundary_points.end());
+
+        scanning_axis_boundary_points.erase(std::unique(scanning_axis_boundary_points.begin(),
+                                                        scanning_axis_boundary_points.end()),
+                                            scanning_axis_boundary_points.end());
+
+		// Collate each boundary point along the axis into boundaries, where each boundary is
+		// implemented as a deque of integers sorted in ascending order from back to front
+        for(int boundary_point : scanning_axis_boundary_points) {
+            if(boundaries.size() > 0) {
+
+                not_found = true;
+                std::size_t index = 0;
+                while(index < boundaries.size() && not_found) {
+                
+                    boundary_crossed_from_back  = boundary_point == boundaries[index].back()  - 1;
+                    boundary_crossed_from_front = boundary_point == boundaries[index].front() + 1;
+
+                    if(boundary_crossed_from_back || boundary_crossed_from_front) {
+
+                    not_found = false;
+
+                        if(boundary_crossed_from_back) {
+                            boundaries[index].push_back(boundary_point);
+                        } else {
+                            boundaries[index].push_front(boundary_point);
+                        }
+                    }
+
+                index++;
+                }
+                if(not_found) {
+                    boundaries.push_back(std::deque<int> {boundary_point});
+                }
+
+            } else {
+                boundaries.push_back(std::deque<int> {boundary_point});
+            }
+        }
+
+		min_boundary_point = \
+			static_cast<std::size_t>(*std::min_element(scanning_axis_boundary_points.begin(),
+													   scanning_axis_boundary_points.end()));
+
+		max_boundary_point = \
+			static_cast<std::size_t>(*std::max_element(scanning_axis_boundary_points.begin(),
+							 						   scanning_axis_boundary_points.end()));
+
+		// This for-loop is the most important loop, and is the main workhorse of this function.
+		// For each point from the minimum to the maximum boundary point along the scan line,
+		// determine if each individual point lies between a pair of boundaries.
+        for(std::size_t point = min_boundary_point; point < max_boundary_point; point++) {
+
+			// If no extra set of already-filled-in coordinates were given, keep going.
+			// If already-filled-in coordinates were actually given, check to make sure that the
+			// current point is not already within those filled-in-coordinates.
+			keep_going = true;
+			if(reorganized_orthogonal_axis_points.size() > 0){
+				keep_going = std::find(reorganized_orthogonal_axis_points[orthogonal_axis_constant].begin(),
+									   reorganized_orthogonal_axis_points[orthogonal_axis_constant].end(),
+						  			   point) == reorganized_orthogonal_axis_points[orthogonal_axis_constant].end();
+			}
+
+			if(keep_going) {
+
+            	num_boundary_crossings = 0;
+            	num_boundary_crossings_reversed = 0;
+
+            	for(std::deque<int> boundary : boundaries) {
+                	if(point > boundary.front()) {
+                    	num_boundary_crossings++;
+                	}
+                	if(point < boundary.back()) {
+                    	num_boundary_crossings_reversed++;
+                	}
+            	}
+
+            	if(num_boundary_crossings % 2 && num_boundary_crossings_reversed % 2) {
+                	if(orthogonal_axis_first) {
+                    	within_shape_points.push_back({orthogonal_axis_constant, static_cast<int>(point)});
+                	} else {
+                    	within_shape_points.push_back({static_cast<int>(point), orthogonal_axis_constant});
+                	}
+            	}
+			}
+        }
+    }
+
+    return within_shape_points;
+}
+
+
 std::vector<std::array<int, 2>> custom_round_double_vectors_to_int_vectors(const std::vector<std::array<double, 2>> double_vertices)
 {
 	std::vector<std::array<int, 2>> int_vertices (double_vertices.size());
@@ -260,12 +410,21 @@ int main( int argc, char* args[] )
 
 	try {
 
+		std::vector<std::array<int, 2>> arrow_boundary_points {custom_calculate_arrow_boundary_points(50, 20, 40, 20, {50, 100}, 30)};
+		std::vector<std::array<int, 2>> arrow_shape_points {custom_scan_lines_for_within_shape_points(false, arrow_boundary_points)};
+		arrow_shape_points = custom_scan_lines_for_within_shape_points(true, arrow_boundary_points, arrow_shape_points);
+
 		SDML::Video::Window canvas {WINDOW_TITLE, WINDOW_AREA};
 		SDML::Video::Renderer paintbrush {canvas};
 		paintbrush.SetDrawingColor(WHITE);
 		paintbrush.DrawEntireTarget();
 		paintbrush.SetDrawingColor(BLACK);
-		paintbrush.DrawPoints(custom_calculate_arrow_boundary_points(50, 20, 40, 20, {50, 100}, 45));
+		paintbrush.DrawPoints(arrow_boundary_points);
+		paintbrush.DrawPoints(arrow_shape_points);
+		/*paintbrush.SetDrawingColor(RED);
+		paintbrush.DrawLine(std::array<std::array<int, 2>, 2> {{{0, 110}, {WINDOW_AREA[0], 110}}});
+		paintbrush.DrawLine(std::array<std::array<int, 2>, 2> {{{96, 0}, {96, WINDOW_AREA[1]}}});
+		paintbrush.DrawLine(std::array<std::array<int, 2>, 2> {{{113, 0}, {113, WINDOW_AREA[1]}}});*/
 		paintbrush.Update();
 		
 		std::optional<std::unique_ptr<SDML::Events::Event>> current_event;
