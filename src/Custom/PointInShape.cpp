050f4270a28ea89349bc9ce0cc776923738b59cb
root <root@DESKTOP-G3SKC0I.>
2024-11-12 13:17:35 -0500

    Simplifying refactors

--- src/Custom/PointInShape.cpp
+++ src/Custom/PointInShape.cpp
@@ -52,126 +52,115 @@ std::vector<std::array<int, 2>> Custom::PointInShape::calculate_polygon_boundary
 }
 
 
-std::vector<std::array<int, 2>> Custom::PointInShape::double_sided_even_odd_ray_casting(bool orthogonal_axis_first,
-                                                                                        const std::vector<std::array<int, 2>>& boundary_points,
-															    	    	            const std::optional<std::vector<std::array<int, 2>>>& orthogonal_axis_points)
+std::map<int, std::vector<int>> map_2D_coordinates_to_scanlines(std::vector<std::array<int, 2>> coordinates,
+                                                                bool orthogonal_axis_first)
 {
-
-	// Initialization of numerous variables needed for up-coming for loops
-
-    bool not_found;
-    bool boundary_crossed_from_back;
-    bool boundary_crossed_from_front;
-
-    std::vector<std::array<int, 2>> within_shape_points;
-    int num_boundary_crossings;
-    int num_boundary_crossings_reversed;
-
     std::size_t orthogonal_axis {static_cast<std::size_t>(orthogonal_axis_first ?  0 : 1)};
-    std::size_t scanning_axis {static_cast<std::size_t>(orthogonal_axis_first ? 1 : 0)};
 
-	std::size_t min_boundary_point;
-	std::size_t max_boundary_point;
-	bool keep_going;
+    std::map<int, std::vector<int>> scanline_per_orthogonal_axis_point;
+    std::size_t scanning_axis = static_cast<std::size_t>(std::abs(static_cast<int>(orthogonal_axis) - 1));
 
-	// Reorganize the boundary points from coordinates into scan line boundary points
-	// mapped to corresponding orthogonal axis points.
-    std::map<int, std::vector<int>> reorganized_boundary_points;
-    for(std::array<int, 2> point : boundary_points) {
-        if(reorganized_boundary_points.contains(point[orthogonal_axis])) {
-            reorganized_boundary_points[point[orthogonal_axis]].push_back(point[scanning_axis]);
+    for(std::array<int, 2> coordinate : coordinates) {
+        if(scanline_per_orthogonal_axis_point.contains(coordinate[orthogonal_axis])) {
+            scanline_per_orthogonal_axis_point[coordinate[orthogonal_axis]].push_back(coordinate[scanning_axis]);
         } else {
-            reorganized_boundary_points.emplace(point[orthogonal_axis], std::vector<int> {point[scanning_axis]});
+            scanline_per_orthogonal_axis_point.emplace(coordinate[orthogonal_axis], std::vector<int> {coordinate[scanning_axis]});
         }
     }
 
-	// If currently existing filled-in shape points were given, reorganize them from
-	// points from coordinates into scan line boundary points mapped to corresponding
-	// orthogonal axis points
-	std::map<int, std::vector<int>> reorganized_orthogonal_axis_points;
-	if(orthogonal_axis_points.has_value()) {
-		for(std::array<int, 2> point : orthogonal_axis_points.value()){
-			if(reorganized_orthogonal_axis_points.contains(point[orthogonal_axis])) {
-				reorganized_orthogonal_axis_points[point[orthogonal_axis]].push_back(point[scanning_axis]);
-			} else {
-				reorganized_orthogonal_axis_points.emplace(point[orthogonal_axis], std::vector<int> {point[scanning_axis]});
-			}
-		}
-		within_shape_points = orthogonal_axis_points.value();
-	}
+    return scanline_per_orthogonal_axis_point;
+}
 
-	// For each orthogonal axis point and it's corresponding scan line boundary points,...
-    for(auto& [orthogonal_axis_constant, scanning_axis_boundary_points] : reorganized_boundary_points) {
 
-        std::vector<std::deque<int>> boundaries;
+std::map<int, std::vector<std::deque<int>>> collate_points_along_scanlines(std::map<int, std::vector<int>> scanlines_per_orthogonal_axis_point)
+{
+    bool not_found;
+    bool boundary_crossed_from_back;
+    bool boundary_crossed_from_front;
+    std::map<int, std::vector<std::deque<int>>> boundaries_per_orthogonal_axis_point;
 
-		// Make sure the scan line boundary points are not repeated/duplicated
+    for(auto& [orthogonal_axis_point, scanline] : scanlines_per_orthogonal_axis_point) {
 
-        std::sort(scanning_axis_boundary_points.begin(),
-                  scanning_axis_boundary_points.end());
+		// Make sure the integers within the scanline are sorted in ascending order with no repeats/duplicates
 
-        scanning_axis_boundary_points.erase(std::unique(scanning_axis_boundary_points.begin(),
-                                                        scanning_axis_boundary_points.end()),
-                                            scanning_axis_boundary_points.end());
+        std::sort(scanline.begin(),
+                  scanline.end());
 
-		// Collate each boundary point along the axis into boundaries, where each boundary is
-		// implemented as a deque of integers sorted in ascending order from back to front
-        for(int boundary_point : scanning_axis_boundary_points) {
-            if(boundaries.size() > 0) {
+        scanline.erase(std::unique(scanline.begin(),
+                                   scanline.end()),
+                       scanline.end());
+
+        for (int boundary : scanline) {
+            if(boundaries_per_orthogonal_axis_point[orthogonal_axis_point].size() > 0) {
 
                 not_found = true;
                 std::size_t index = 0;
-                while(index < boundaries.size() && not_found) {
+                while(index < boundaries_per_orthogonal_axis_point[orthogonal_axis_point].size() && not_found) {
                 
-                    boundary_crossed_from_back  = boundary_point == boundaries[index].back()  - 1;
-                    boundary_crossed_from_front = boundary_point == boundaries[index].front() + 1;
+                    boundary_crossed_from_back  = boundary == boundaries_per_orthogonal_axis_point[orthogonal_axis_point][index].back()  - 1;
+                    boundary_crossed_from_front = boundary == boundaries_per_orthogonal_axis_point[orthogonal_axis_point][index].front() + 1;
 
                     if(boundary_crossed_from_back || boundary_crossed_from_front) {
 
                     not_found = false;
 
                         if(boundary_crossed_from_back) {
-                            boundaries[index].push_back(boundary_point);
+                            boundaries_per_orthogonal_axis_point[orthogonal_axis_point][index].push_back(boundary);
                         } else {
-                            boundaries[index].push_front(boundary_point);
+                            boundaries_per_orthogonal_axis_point[orthogonal_axis_point][index].push_front(boundary);
                         }
                     }
 
                 index++;
                 }
                 if(not_found) {
-                    boundaries.push_back(std::deque<int> {boundary_point});
+                    boundaries_per_orthogonal_axis_point[orthogonal_axis_point].push_back(std::deque<int> {boundary});
                 }
 
             } else {
-                boundaries.push_back(std::deque<int> {boundary_point});
+                boundaries_per_orthogonal_axis_point[orthogonal_axis_point].push_back(std::deque<int> {boundary});
             }
         }
 
+    }
+
+    return boundaries_per_orthogonal_axis_point;
+}
+
+
+std::vector<std::array<int, 2>> Custom::PointInShape::double_sided_even_odd_ray_casting(bool orthogonal_axis_first,
+                                                                                        const std::vector<std::array<int, 2>>& boundary_coordinates,
+															    	    	            const std::optional<std::vector<std::array<int, 2>>>& orthogonal_axis_points)
+{
+    std::vector<std::array<int, 2>> within_shape_points;
+    if(orthogonal_axis_points.has_value()) {
+        within_shape_points = orthogonal_axis_points.value();
+    }
+    int num_boundary_crossings;
+    int num_boundary_crossings_reversed;
+
+	std::size_t min_boundary_point;
+	std::size_t max_boundary_point;
+
+    std::map<int, std::vector<int>> scanlines_per_orthogonal_axis_point {map_2D_coordinates_to_scanlines(boundary_coordinates, orthogonal_axis_first)};
+    std::map<int, std::vector<std::deque<int>>> scanline_boundaries_per_orthogonal_line {collate_points_along_scanlines(scanlines_per_orthogonal_axis_point)};
+	std::optional<std::map<int, std::vector<int>>> reorganized_orthogonal_axis_points {orthogonal_axis_points.has_value() ? std::make_optional(map_2D_coordinates_to_scanlines(orthogonal_axis_points.value(), orthogonal_axis_first)) : std::nullopt};
+
+    for(auto& [orthogonal_axis_point, boundaries] : scanline_boundaries_per_orthogonal_line) {
+
 		min_boundary_point = \
-			static_cast<std::size_t>(*std::min_element(scanning_axis_boundary_points.begin(),
-													   scanning_axis_boundary_points.end()));
+			static_cast<std::size_t>(*std::min_element(scanlines_per_orthogonal_axis_point[orthogonal_axis_point].begin(),
+													   scanlines_per_orthogonal_axis_point[orthogonal_axis_point].end()));
 
 		max_boundary_point = \
-			static_cast<std::size_t>(*std::max_element(scanning_axis_boundary_points.begin(),
-							 						   scanning_axis_boundary_points.end()));
+			static_cast<std::size_t>(*std::max_element(scanlines_per_orthogonal_axis_point[orthogonal_axis_point].begin(),
+							 						   scanlines_per_orthogonal_axis_point[orthogonal_axis_point].end()));
 
-		// This for-loop is the most important loop, and is the main workhorse of this function.
-		// For each point from the minimum to the maximum boundary point along the scan line,
-		// determine if each individual point lies between a pair of boundaries.
         for(std::size_t point = min_boundary_point; point < max_boundary_point; point++) {
 
-			// If no extra set of already-filled-in coordinates were given, keep going.
-			// If already-filled-in coordinates were actually given, check to make sure that the
-			// current point is not already within those filled-in-coordinates.
-			keep_going = true;
-			if(reorganized_orthogonal_axis_points.size() > 0){
-				keep_going = std::find(reorganized_orthogonal_axis_points[orthogonal_axis_constant].begin(),
-									   reorganized_orthogonal_axis_points[orthogonal_axis_constant].end(),
-						  			   point) == reorganized_orthogonal_axis_points[orthogonal_axis_constant].end();
-			}
-
-			if(keep_going) {
+			if(reorganized_orthogonal_axis_points.has_value() ? std::find(reorganized_orthogonal_axis_points.value()[orthogonal_axis_point].begin(),
+                                                                          reorganized_orthogonal_axis_points.value()[orthogonal_axis_point].end(),
+                                                                          point) == reorganized_orthogonal_axis_points.value()[orthogonal_axis_point].end() : true) {
 
             	num_boundary_crossings = 0;
             	num_boundary_crossings_reversed = 0;
@@ -187,9 +176,9 @@ std::vector<std::array<int, 2>> Custom::PointInShape::double_sided_even_odd_ray_
 
             	if(num_boundary_crossings % 2 && num_boundary_crossings_reversed % 2) {
                 	if(orthogonal_axis_first) {
-                    	within_shape_points.push_back({orthogonal_axis_constant, static_cast<int>(point)});
+                    	within_shape_points.push_back({orthogonal_axis_point, static_cast<int>(point)});
                 	} else {
-                    	within_shape_points.push_back({static_cast<int>(point), orthogonal_axis_constant});
+                    	within_shape_points.push_back({static_cast<int>(point), orthogonal_axis_point});
                 	}
             	}
 			}
