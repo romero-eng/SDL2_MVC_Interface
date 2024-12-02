179b9b224c75928da41ead9e99a51e9ec6b571e1
root <root@DESKTOP-G3SKC0I.>
2024-12-02 14:14:12 -0500

    Minor change

--- src/Main.cpp
+++ src/Main.cpp
@@ -28,6 +28,13 @@ constexpr std::array<uint8_t, 3> WHITE {0xFF, 0xFF, 0xFF};
 constexpr std::array<uint8_t, 3> BLACK {0x00, 0x00, 0x00};
 constexpr std::array<uint8_t, 3> RED {0xFF, 0x00, 0x00};
 
+constexpr double W_r = 50;
+constexpr double W_t = 20;
+constexpr double H_r = 40;
+constexpr double H_t = 20;
+constexpr std::array<double, 2> center = {50, 100};
+constexpr double angle_degrees = 30;
+
 
 int main( int argc, char* args[] )
 {
@@ -35,13 +42,6 @@ int main( int argc, char* args[] )
 	SDML::Subsystem::Initialize(LOGFILE_NAME, SDML::Subsystem::InitFlag::VIDEO | SDML::Subsystem::InitFlag::EVENTS);
 
 	try {
-
-		constexpr double W_r = 50;
-		constexpr double W_t = 20;
-		constexpr double H_r = 40;
-		constexpr double H_t = 20;
-		constexpr std::array<double, 2> center = {50, 100};
-		constexpr double angle_degrees = 30;
 
 		SDML::Video::Window canvas {WINDOW_TITLE, WINDOW_AREA};
 		SDML::Video::Renderer paintbrush {canvas};
@@ -52,15 +52,15 @@ int main( int argc, char* args[] )
 		std::vector<std::array<int, 2>> arrow_within_boundary_points;
 		std::tie(arrow_boundary_points,
 				 arrow_within_boundary_points) = \
-				 Custom::Polygon::calculate_points(Custom::LinearAlgebra::round_double_vectors_to_int_vectors(Custom::LinearAlgebra::translate_and_rotate_vectors({{		      0, 	    H_r},
-																																								 {(W_r - W_t)/2, 	    H_r},
-																		 																						 {(W_r - W_t)/2,          0},
-																	 																							 {(W_r + W_t)/2,   	      0},
-																	 																							 {(W_r + W_t)/2, 	    H_r},
-																	 																							 {	   	     W_r, 	    H_r},
-																	 																							 {	 	   W_r/2, H_r + H_t}},
-																																								angle_degrees,
-																																								center)));
+				 Custom::Polygon::calculate_points(Custom::LinearAlgebra::round_double_vectors_to_int_vectors(Custom::LinearAlgebra::translate_and_rotate_vectors({{		    0, 	     H_r},
+																																								   {(W_r - W_t)/2, 	     H_r},
+																		 																						   {(W_r - W_t)/2,         0},
+																	 																							   {(W_r + W_t)/2,   	   0},
+																	 																							   {(W_r + W_t)/2, 	     H_r},
+																	 																							   {	   	W_r  , 	     H_r},
+																	 																							   {	 	W_r/2, H_r + H_t}},
+																																								  angle_degrees,
+																																								  center)));
 		paintbrush.SetDrawingColor(RED);
 		paintbrush.DrawPoints(arrow_within_boundary_points);
 		paintbrush.SetDrawingColor(BLACK);
