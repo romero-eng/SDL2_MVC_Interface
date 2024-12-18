13e438ccea45f8c8652db9174f529f9de627b42d
root <root@DESKTOP-G3SKC0I.>
2024-12-18 16:07:44 -0500

    Reorganized coefficients

--- rotated_ellipse.py
+++ rotated_ellipse.py
@@ -8,17 +8,18 @@ def plot_ellipse(a, b, theta_deg, square_lim, x_step, save=True):
 
     norm_sq = (a*a - b*b)*np.square(np.cos(theta_rad)) + b*b
 
-    A = (a*a/2)*np.sin(2*theta_rad)/norm_sq
-    B = a*b*np.square(np.sin(theta_rad))/norm_sq
-    G = a*a*b*b*np.square(np.square(np.cos(theta_rad)))/norm_sq
-    H = (b*b)*(a*a + b*b)*(np.square(np.square(np.cos(theta_rad)) - (b*b/(2*(a*a + b*b)))) - np.square(b*b/(2*(a*a + b*b))))/np.square(norm_sq)
-    K = (a*b*b*b/2)*np.sin(2*theta_rad)*np.square(np.cos(theta_rad)/norm_sq)
+    coeffs = \
+        [(a*a/2)*np.sin(2*theta_rad)/norm_sq,
+         a*b*np.square(np.sin(theta_rad))/norm_sq,
+         a*a*b*b*np.square(np.square(np.cos(theta_rad)))/norm_sq,
+         (b*b)*(a*a + b*b)*(np.square(np.square(np.cos(theta_rad)) - (b*b/(2*(a*a + b*b)))) - np.square(b*b/(2*(a*a + b*b))))/np.square(norm_sq),
+         (a*b*b*b/2)*np.sin(2*theta_rad)*np.square(np.cos(theta_rad)/norm_sq)]
 
     upper = 1 if theta_rad < np.pi else -1
     right = 1 if theta_rad <= np.pi/2 or theta_rad >= 3*np.pi/2 else -1
     sgn = lambda x, sign : -np.sign(x - upper*sign*a*np.cos(theta_rad))*upper*right
 
-    y = lambda x, sign: A*x + sign*B*np.sqrt(norm_sq - np.square(x)) + sgn(x, sign)*np.sqrt(G - H*np.square(x) - 2*sign*K*x*np.sqrt(norm_sq - np.square(x)))
+    y = lambda x, sign: coeffs[0]*x + sign*coeffs[1]*np.sqrt(norm_sq - np.square(x)) + sgn(x, sign)*np.sqrt(coeffs[2] - coeffs[3]*np.square(x) - 2*sign*coeffs[4]*x*np.sqrt(norm_sq - np.square(x)))
 
     fig, axs = plt.subplots(1)
 
@@ -47,17 +48,18 @@ def plot_ellipse_v2(a, b, theta_deg, square_lim, y_step, save=True):
 
     norm_sq = (b*b - a*a)*np.square(np.cos(theta_rad)) + np.square(a)
 
-    F = (b*b/2)*np.sin(2*theta_rad)/norm_sq
-    E = a*b*np.square(np.sin(theta_rad))/norm_sq
-    L = a*a*b*b*np.square(np.square(np.cos(theta_rad)))/norm_sq
-    M = a*a*(a*a + b*b)*(np.square(np.square(np.cos(theta_rad)) - (a*a/(2*(a*a + b*b)))) - np.square(a*a/(2*(a*a + b*b))))/np.square(norm_sq)
-    N = (a*a*a*b/2)*np.sin(2*theta_rad)*np.square(np.cos(theta_rad)/norm_sq)
+    coeffs = \
+        [(b*b/2)*np.sin(2*theta_rad)/norm_sq,
+         a*b*np.square(np.sin(theta_rad))/norm_sq,
+         a*a*b*b*np.square(np.square(np.cos(theta_rad)))/norm_sq,
+         a*a*(a*a + b*b)*(np.square(np.square(np.cos(theta_rad)) - (a*a/(2*(a*a + b*b)))) - np.square(a*a/(2*(a*a + b*b))))/np.square(norm_sq),
+         (a*a*a*b/2)*np.sin(2*theta_rad)*np.square(np.cos(theta_rad)/norm_sq)]
 
     lower = 1 if theta_rad > np.pi else -1
     right = 1 if theta_rad <= np.pi/2 or theta_rad >= 3*np.pi/2 else -1
     sgn = lambda y, sign : -np.sign(y - lower*sign*b*np.cos(theta_rad))*lower*right
 
-    x = lambda y, sign: -F*y + sign*E*np.sqrt(norm_sq - np.square(y)) + sgn(y, sign)*np.sqrt(L - M*np.square(y) + 2*sign*N*y*np.sqrt(norm_sq - np.square(y)))
+    x = lambda y, sign: -coeffs[0]*y + sign*coeffs[1]*np.sqrt(norm_sq - np.square(y)) + sgn(y, sign)*np.sqrt(coeffs[2] - coeffs[3]*np.square(y) + 2*sign*coeffs[4]*y*np.sqrt(norm_sq - np.square(y)))
 
     fig, axs = plt.subplots(1)
 
